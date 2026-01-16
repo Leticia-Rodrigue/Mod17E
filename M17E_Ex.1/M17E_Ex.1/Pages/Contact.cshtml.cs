bf32f675e8cd52bae88da3bfed6c149b9f430db7
Letícia Rodrigues <12.leticia.rodrigues@gmail.com>
2026-01-16 13:47:48 +0000

    1.ºCommit

---
+++ M17E_Ex.1/M17E_Ex.1/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace M17E_Ex._1.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ M17E_Ex.1/M17E_Ex.1/Pages/Historia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace M17E_Ex._1.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
